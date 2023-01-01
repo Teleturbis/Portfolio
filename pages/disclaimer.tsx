212c5924d2d475abc88a5d1cc13cd46ba9ef5d7e
Kevin Poppe <Kevin@KevinPoppe.com>
2023-01-01 20:38:46 +0100

    fixed bug

--- pages/disclaimer.tsx
+++ pages/disclaimer.tsx
@@ -96,12 +96,12 @@ export default function App() {
             </p>
             <br />
             <br />
+            <h3>
+              <strong>Haftungsausschluss:</strong>
+            </h3>
+            <br />
+            <br />
             <p>
-              <h3>
-                <strong>Haftungsausschluss:</strong>
-              </h3>
-              <br />
-              <br />
               <strong>Haftung für Links</strong>
               <br />
               <br />
@@ -162,28 +162,29 @@ export default function App() {
               <br />
               <br />
               Diese Website benutzt Google Analytics, einen Webanalysedienst der
-              Google Inc. ("Google"). Google Analytics verwendet sog. "Cookies",
-              Textdateien, die auf Ihrem Computer gespeichert werden und die
-              eine Analyse der Benutzung der Website durch Sie ermöglicht. Die
-              durch den Cookie erzeugten Informationen über Ihre Benutzung
-              dieser Website (einschließlich Ihrer IP-Adresse) wird an einen
-              Server von Google in den USA übertragen und dort gespeichert.
-              Google wird diese Informationen benutzen, um Ihre Nutzung der
-              Website auszuwerten, um Reports über die Websiteaktivitäten für
-              die Websitebetreiber zusammenzustellen und um weitere mit der
-              Websitenutzung und der Internetnutzung verbundene Dienstleistungen
-              zu erbringen. Auch wird Google diese Informationen gegebenenfalls
-              an Dritte übertragen, sofern dies gesetzlich vorgeschrieben oder
-              soweit Dritte diese Daten im Auftrag von Google verarbeiten.
-              Google wird in keinem Fall Ihre IP-Adresse mit anderen Daten der
-              Google in Verbindung bringen. Sie können die Installation der
-              Cookies durch eine entsprechende Einstellung Ihrer Browser
-              Software verhindern; wir weisen Sie jedoch darauf hin, dass Sie in
-              diesem Fall gegebenenfalls nicht sämtliche Funktionen dieser
-              Website voll umfänglich nutzen können. Durch die Nutzung dieser
-              Website erklären Sie sich mit der Bearbeitung der über Sie
-              erhobenen Daten durch Google in der zuvor beschriebenen Art und
-              Weise und zu dem zuvor benannten Zweck einverstanden.
+              Google Inc. ({'"'}Google{'"'}). Google Analytics verwendet sog.{' '}
+              {'"'}Cookies{'"'}, Textdateien, die auf Ihrem Computer gespeichert
+              werden und die eine Analyse der Benutzung der Website durch Sie
+              ermöglicht. Die durch den Cookie erzeugten Informationen über Ihre
+              Benutzung dieser Website (einschließlich Ihrer IP-Adresse) wird an
+              einen Server von Google in den USA übertragen und dort
+              gespeichert. Google wird diese Informationen benutzen, um Ihre
+              Nutzung der Website auszuwerten, um Reports über die
+              Websiteaktivitäten für die Websitebetreiber zusammenzustellen und
+              um weitere mit der Websitenutzung und der Internetnutzung
+              verbundene Dienstleistungen zu erbringen. Auch wird Google diese
+              Informationen gegebenenfalls an Dritte übertragen, sofern dies
+              gesetzlich vorgeschrieben oder soweit Dritte diese Daten im
+              Auftrag von Google verarbeiten. Google wird in keinem Fall Ihre
+              IP-Adresse mit anderen Daten der Google in Verbindung bringen. Sie
+              können die Installation der Cookies durch eine entsprechende
+              Einstellung Ihrer Browser Software verhindern; wir weisen Sie
+              jedoch darauf hin, dass Sie in diesem Fall gegebenenfalls nicht
+              sämtliche Funktionen dieser Website voll umfänglich nutzen können.
+              Durch die Nutzung dieser Website erklären Sie sich mit der
+              Bearbeitung der über Sie erhobenen Daten durch Google in der zuvor
+              beschriebenen Art und Weise und zu dem zuvor benannten Zweck
+              einverstanden.
             </p>
             <br />
             Website Impressum von{' '}
