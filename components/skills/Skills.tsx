895d7755077a870b6e8331cb2150e90c5da493ef
Kevin Poppe <Kevin@KevinPoppe.com>
2023-01-01 17:13:53 +0100

    wip mediaquerry

--- components/skills/Skills.tsx
+++ components/skills/Skills.tsx
@@ -68,13 +68,14 @@ export default function About() {
 
   return (
     <main className={`${styles.main}`}>
+      <h2>Meine Tech-Skills</h2>
       <motion.div
         className={styles.skillsDiv}
-        initial={{ scale: 0, rotate: 720 }}
+        initial={{ scale: 0, rotate: 0 }}
         animate={{ rotate: 0, scale: 1 }}
         transition={{
-          delay: 1,
-          duration: 2,
+          delay: 0,
+          duration: 0.5,
           type: 'spring',
         }}
         onClick={() => setIsOpen((_open) => !_open)}
@@ -82,16 +83,16 @@ export default function About() {
         {POSITIONS.map(([icon, v1, t1, v2, t2], index) => (
           <motion.div
             key={index}
-            initial={{ opacity: 0 }}
+            initial={{ opacity: 1 }}
             animate={{
-              [v1]: isOpen ? (isBreakpoint ? t1 / 2 : t1) : 0,
-              [v2]: isOpen ? (isBreakpoint ? t2 / 2 : t2) : 0,
-              opacity: isOpen ? 1 : 0,
-              scale: isOpen ? 1 : 0,
+              [v1]: isOpen ? 0 : isBreakpoint ? t1 / 2 : t1,
+              [v2]: isOpen ? 0 : isBreakpoint ? t2 / 2 : t2,
+              opacity: isOpen ? 0 : 1,
+              scale: isOpen ? 0 : 1,
             }}
             transition={{
               delay: 0.08 * index,
-              duration: 0.2,
+              duration: 0,
               type: 'spring',
               damping: 12,
             }}
